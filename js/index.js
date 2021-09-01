f6c8340954c65943fa8990319603d965c7456db0
kcyuan93 <76415009+kcyuan93@users.noreply.github.com>
2021-09-01 01:53:44 -0400

    edited about page and button
    back to top button

--- js/index.js
+++ js/index.js
@@ -176,3 +176,20 @@ $('.js-anchor-link').click(function(e){
     $('body, html').animate({scrollTop: scrollTo+'px'}, 800);
   }
 });
+
+// <!-- Back to top button -->
+
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
